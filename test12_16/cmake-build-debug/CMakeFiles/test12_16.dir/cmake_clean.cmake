252359c917803ea5612d4fceef6212e50aea585a
iggzq <2811328244@qq.com>
2021-12-16 20:35:29 +0800

    12_16

---
+++ test12_16/cmake-build-debug/CMakeFiles/test12_16.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/test12_16.dir/main.c.obj"
+  "libtest12_16.dll.a"
+  "test12_16.exe"
+  "test12_16.exe.manifest"
+  "test12_16.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/test12_16.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ test12_16/main.c
@@ -0,0 +1,113 @@
+#include <stdio.h>
+
+/*int main() {
+    int i = 0;
+    int j = 0;
+    for(i = 1; i<=7; i=i+2)
+    {
+        for(j = 1; j<=i; j++)
+        {
+            printf("*");
+        }
+        printf("\n");
+    }
+    for(i = 5; i>=1; i = i - 2)
+    {
+        for(j = 1; j<=i; j++)
+        {
+            printf("*");
+        }
+        printf("\n");
+    }
+    return 0;
+}
+*/
+/*main()
+{
+    int n;
+    int m;
+    int i = 0;
+    int j = 0;
+    int o = 0;
+    scanf("%d",&n);
+    for(i = 2; i<n; i++)
+    {
+        m = n - i;
+        for(j = 2; j<m; j++)
+        {
+            if(m%j!=0)
+            {
+                o = 1;
+            }
+            else
+            {
+                o = 2;
+                break;
+            }
+        }
+        if(o == 1&&i % j!=0)
+        {
+            printf("%d = %d + %d\n",n,m,i);
+            break;
+        }
+    }
+    return 0;
+}*/
+/*main()
+{
+    int i = 0;
+    int j = 0;
+    for(i = 1; i<=9; i++)
+    {
+        for(j = 1; j<=i; j++)
+        {
+            printf("%d*%d=%d ",j,i,i*j);
+        }
+        printf("\n");
+    }
+}*/
+main()
+{
+    double x = 0.1;
+    double s = 0;
+    double i = 1;
+    double m = 1;
+    //scanf("%lf",&x);
+    while(x/m >0.00001)
+    {
+        m = m * i;
+        s = s + x/m;
+        x = x*x;
+        i++;
+    }
+    printf("%lf",s);
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
