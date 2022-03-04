e11297ca0d1bdb252ccd880c8becec914802e16a
iggzq <2811328244@qq.com>
2022-03-04 21:13:22 +0800

    memory

---
+++ memory/cmake-build-debug/CMakeFiles/memory.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/memory.dir/main.cpp.obj"
+  "libmemory.dll.a"
+  "memory.exe"
+  "memory.exe.manifest"
+  "memory.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/memory.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ memory/main.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <stdio.h>
+#include "stdlib.h"
+#include "string.h"
+//int main() {
+//    int* p = (int*)malloc(sizeof (int)*10);
+//    int i = 0;
+//    if( p == NULL)
+//    {
+//        printf("%s\n",strerror(errno));
+//    }
+//    for(i = 0; i < 10; i++)
+//    {
+//        *(p+i) = i;
+//    }
+//    for(i = 0; i < 10; i++)
+//    {
+//        printf("%d ",*(p+i));
+//    }
+//    free(p);
+//    p = NULL;
+//    return  0;
+//}
+//
